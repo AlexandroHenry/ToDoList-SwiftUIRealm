d086aff65bb1405a8ba594147f2098cc961ee47e
alaxhenry <alaxhenry0121@gmail.com>
2022-12-19 12:31:36 +0900

    Initial Commit

---
+++ ToDoList-SwiftUIRealm/ToDoList_SwiftUIRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_SwiftUIRealmApp.swift
+//  ToDoList-SwiftUIRealm
+//
+//  Created by Seungchul Ha on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_SwiftUIRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
